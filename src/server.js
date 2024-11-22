a20e53a984cfb39e0e507a6e0146f92c7cae4267
mohammedbilalns <bilalnsmuhammed@gmail.com>
2024-11-22 16:58:46 +0530

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import express from "express"
+import { configDotenv } from "dotenv"
+import {log} from "mercedlogger"
+
+configDotenv()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.set("view engine", "ejs")
+
+app.use(express.static('static'))
+
+app.get('/', (req,res)=>{
+    res.render("./user/home.ejs")
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{  
+    log.green('SERVER STATUS', `server running on port: ${PORT}`)
+})
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/**/*.ejs", "./src/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
