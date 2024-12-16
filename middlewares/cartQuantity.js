43a3d2bc79acfb88d5be1e279242b4547b711fd1
mohammedbilalns <bilalnsmuhammed@gmail.com>
2024-12-16 18:01:18 +0530

    update: disable the decrement quantity button in the orders section when the quantity is one

--- middlewares/cartQuantity.js
+++ middlewares/cartQuantity.js
@@ -9,7 +9,6 @@ const fetchCartQuantity = async (req,res, next)=>{
             const cart = await cartSchema.findOne({user: userid})
             const totalQuantity = cart.items.reduce((sum , item)=> sum+ item.quantity,0)
             res.locals.cartQuantity = totalQuantity
-            console.log("reached line 15 ")
         }else {
             res.locals.cartQuantity = 0 
         }
