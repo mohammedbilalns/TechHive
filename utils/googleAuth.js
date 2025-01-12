f0578282a153114e8b74dbd755aac3be5068672e
mohammedbilalns <bilalnsmuhammed@gmail.com>
2025-01-12 10:12:28 +0530

    Update googleAuth.js

--- utils/googleAuth.js
+++ utils/googleAuth.js
@@ -7,7 +7,7 @@ configDotenv();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
 async (token, tokenSecret, profile, done) => {
     try {
