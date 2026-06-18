8b5af235ea83cc529854baef5c81e1ba8e7e24ea
mohammedbilalns <70318383+mohammedbilalns@users.noreply.github.com>
2026-06-18 18:24:50 +0530

    log request content type

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,10 +2,19 @@ import logger from "../utils/logger.js";
 
 export const errorMiddleware = (err, req, res, _next) => {
   void _next;
-  logger.error("GLOBAL_ERROR");
-  logger.error("Name:", err.name);
-  logger.error("Message:", err.message);
-  logger.error("Stack:", err.stack);
+  logger.error("GLOBAL_ERROR", {
+    requestId: req.requestId || req.res?.locals?.requestId || null,
+    method: req.method,
+    url: req.originalUrl,
+    statusCode: err.statusCode || 500,
+    name: err.name,
+    message: err.message,
+    code: err.code,
+    field: err.field,
+    storageErrors: err.storageErrors,
+    stack: err.stack,
+    contentType: req.headers["content-type"],
+  });
 
   const statusCode = err.statusCode || 500;
   const message = err.isOperational || statusCode !== 500 ? err.message : "Internal Server Error";
